74498bba326bbcca5df4e7bf744480a7f2cdd841
apayne1297 <42837001+CrashTest500@users.noreply.github.com>
2024-06-16 23:21:48 -0400

    inital checking of working app

---
+++ observables-demo/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ScoreboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'observables-demo';
+}
